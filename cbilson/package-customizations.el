71ccb807a98fb31e3bba2d2ceaadbbd06a2f6322
Chris Bilson <cbilson@pobox.com>
2011-01-13 04:41:57 -0800

    add packages I was installing manually

--- cbilson/package-customizations.el
+++ cbilson/package-customizations.el
@@ -2,6 +2,25 @@
 ;;
 ;; Package Management
 ;;
-(add-to-list 'package-archives
-             '("technomancy" . "http://repo.technomancy.us/emacs/") t)
+
+;; additional packages I want installed
+(dolist (package '(clojure-mode
+                   clojure-test-mode
+                   color-theme
+                   dired-isearch
+                   haml-mode
+                   js2-mode
+                   markdown-mode
+                   muse
+                   org
+                   paredit
+                   rinari
+                   rspec-mode
+                   ruby-electric
+                   rvm
+                   sass-mode
+                   slime
+                   yasnippet-bundle
+                   zenburn))
+  (add-to-list 'starter-kit-packages package))
 
