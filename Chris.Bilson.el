d1b2b46e2a3950886853d94ea83180cf5e1cf414
Chris Bilson <cbilson@pobox.com>
2011-01-13 04:48:19 -0800

    Add Chris.Bilson.el user file that changes the user-specific-dir to cbilson

---
+++ Chris.Bilson.el
@@ -0,0 +1,7 @@
+;; On some computers, my login is Chris.Bilson
+(setq user-specific-dir (concat dotfiles-dir "cbilson"))
+
+(if (file-exists-p user-specific-dir)
+    (progn
+      (add-to-list 'load-path user-specific-dir)
+      (mapc #'load (directory-files user-specific-dir nil "*.el$"))))
