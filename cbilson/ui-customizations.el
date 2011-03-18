5d34cee51cd02957894ef0227377e867a525a5f8
Chris Bilson <chris@rigel.(none)>
2011-03-18 07:08:41 -0700

    add nicer toggle-fullscreen function

--- cbilson/ui-customizations.el
+++ cbilson/ui-customizations.el
@@ -7,3 +7,11 @@
       (require 'color-theme-sunburst)
       (color-theme-sunburst)))
 
+;; stolen from https://github.com/nimms/emacs-starter-kit/commit/b43f4f7e5f885c6f2c804e6221fe5c99db4908db
+(defun toggle-fullscreen ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                         'fullboth)))
+
+(global-set-key [(meta return)] 'toggle-fullscreen)
