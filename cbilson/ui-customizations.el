33a7884cc7570450fe6f014a84963396d4ecb63e
Chris Bilson <chris@rigel.(none)>
2011-03-18 06:59:30 -0700

    add more color themes

--- cbilson/ui-customizations.el
+++ cbilson/ui-customizations.el
@@ -2,6 +2,8 @@
 (if window-system
     (progn
       (require 'color-theme)
+      (add-to-list 'load-path (concat dotfiles-dir "cbilson/themes"))
+      (load (concat dotfiles-dir "cbilson/themes/color-theme-library"))
       (require 'color-theme-sunburst)
       (color-theme-sunburst)))
 
