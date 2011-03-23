b34c564a811784538565ee7484bc20fa93258aea
Chris Bilson <cbilson@pobox.com>
2011-03-23 08:37:27 -0700

    use a higher contrast dark theme
    It's light on the train in the morning now, so I need more contrast

--- cbilson/ui-customizations.el
+++ cbilson/ui-customizations.el
@@ -4,8 +4,11 @@
       (require 'color-theme)
       (add-to-list 'load-path (concat dotfiles-dir "cbilson/themes"))
       (load (concat dotfiles-dir "cbilson/themes/color-theme-library"))
-      (require 'color-theme-sunburst)
-      (color-theme-sunburst)))
+      ;(color-theme-arjen)
+      (color-theme-clarity)
+      ;(color-theme-dark-blue2)
+      ;(color-theme-sunburst)
+      ))
 
 ;; stolen from https://github.com/nimms/emacs-starter-kit/commit/b43f4f7e5f885c6f2c804e6221fe5c99db4908db
 (defun toggle-fullscreen ()
